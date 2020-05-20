828d54c594a36314d5793bf2caea7052d6e5ce16
SedemQuame <sedem.amekpewu.3@gmail.com>
2020-05-20 22:57:50 +0000

    shared preferences, final edit.

--- Eros-Android/app/src/main/java/com/visionarytech/eros/Activities/SignUpActivity.java
+++ Eros-Android/app/src/main/java/com/visionarytech/eros/Activities/SignUpActivity.java
@@ -59,27 +59,23 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 //            use material snackBar here
             return;
         }
-
         if (!Patterns.EMAIL_ADDRESS.matcher(emailAddress).matches()) {
             emailEditText.setError("Email is not valid");
             emailEditText.requestFocus();
 //            use material snackBar here
             return;
         }
-
         if (password.isEmpty()) {
             passwordEditText.setError("Password is required");
             passwordEditText.requestFocus();
 //            use material snackBar here
             return;
         }
-
         if (password.length() < 8) {
             passwordEditText.setError("Password should be at least 8 characters");
             passwordEditText.requestFocus();
             return;
         }
-
         mAuth.createUserWithEmailAndPassword(emailAddress, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
@@ -94,17 +90,16 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 //                      Saving New User Preferences.
                     editor.apply();
 //                      Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "createUserWithEmail:success");
+//                    Log.d(TAG, "createUserWithEmail:success");
                     Intent intent = new Intent(getApplicationContext(), ProfileRegistration.class);
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     startActivity(intent);
                     finish();
-                    Toast.makeText(SignUpActivity.this, "Authentication succeeded.",
-                            Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(SignUpActivity.this, "Authentication succeeded.",
+//                            Toast.LENGTH_SHORT).show();
                 } else {
                     // If sign in fails, display a message to the user.
-                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
-
+//                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
                     if (task.getException() instanceof FirebaseAuthUserCollisionException) {
                         Toast.makeText(getApplicationContext(), "Email already registered.",
                                 Toast.LENGTH_SHORT).show();
--- Eros-Android/app/src/main/java/com/visionarytech/eros/Fragments/ProfileFragment.java
+++ Eros-Android/app/src/main/java/com/visionarytech/eros/Fragments/ProfileFragment.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +18,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.visionarytech.eros.Activities.LoginActivity;
 import com.visionarytech.eros.R;
 
@@ -25,6 +32,8 @@ import com.visionarytech.eros.R;
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment implements View.OnClickListener {
+    SharedPreferences sharedPref;
+    private static final String TAG = "ProfileFragment";
     View v;
     private TextView username;
 
@@ -38,6 +47,7 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
     private EditText email;
     private EditText phone;
 
+    private Button deleteAccount;
     private Button logOut;
     private FirebaseAuth mAuth;
 
@@ -63,7 +73,7 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
 
 //        Setting value for editTextViews in fragment with values from shared preferences.
         Context context = getActivity();
-        SharedPreferences sharedPref = context.getSharedPreferences(getString(R.string.shared_preferences_of_user), context.MODE_PRIVATE);
+        sharedPref = context.getSharedPreferences(getString(R.string.shared_preferences_of_user), context.MODE_PRIVATE);
 
         username.setText(sharedPref.getString("Username", null));
 
@@ -76,8 +86,9 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         phone.setText(sharedPref.getString("Phone", null));
 
         logOut = v.findViewById(R.id.logOutButton);
+        deleteAccount = v.findViewById(R.id.deleteAccountButton);
         logOut.setOnClickListener(this);
-
+        deleteAccount.setOnClickListener(this);
         return v;
     }
 
@@ -87,7 +98,27 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
             case (R.id.logOutButton):
                 mAuth.signOut();
                 Intent intent = new Intent(getContext(), LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
+                break;
+            case (R.id.deleteAccountButton):
+//                mAuth.signOut();
+                FirebaseUser user = mAuth.getCurrentUser();
+//                todo: Must work on successfully deleting user account.
+                user.delete()
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d(TAG, "onComplete: Account deleted.");
+                                    Intent intent = new Intent(getContext(), LoginActivity.class);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    startActivity(intent);
+                                }else{
+                                    Log.d(TAG, "onComplete: Account not deleted.");
+                                }
+                            }
+                        });
                 break;
         }
     }
